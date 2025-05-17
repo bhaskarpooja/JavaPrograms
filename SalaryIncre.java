1f97003f4745042016df0884781f571910f80095
Pooja Bhaskar <poojabhaskar529@gmail.com>
2025-05-18 00:07:27 +0530

    Add files via upload

---
+++ SalaryIncre.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class SalaryIncre{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		System.out.println("Enter your salary : ");
+		double sal=sc.nextDouble();
+		System.out.println("Enter your score : ");
+		int score=sc.nextInt();
+		if(score>0 && score<100){
+           if(score>=90){
+           	System.out.println("previous salary : "+sal);
+           	sal+=(sal*0.03);
+           	System.out.println("Incremented salary by 3% is : "+sal+" rs.");
+           }
+           else{
+            System.out.println("previous salary : "+sal);
+           	sal+=(sal*0.01);
+           	System.out.println("Incremented salary by 1% is : "+sal+" rs.");
+           }
+		}
+		else{
+			System.out.println("Please enter valid score");
+		}
+	}
+}
